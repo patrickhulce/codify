0260313b067e39c61c88cd5f7e7439ea7e78aa6c
Patrick Hulce <patrick.hulce@gmail.com>
2013-09-07 20:21:14 -0400

    Added facebook auth

--- js/app/app.js
+++ js/app/app.js
@@ -1,12 +1,12 @@
 var app = {
-	settings : {
-		'firebaseUrl': 'https://codify.firebaseio.com/'
-	}
+    settings: {
+        'firebaseUrl': 'https://codify.firebaseio.com/'
+    }
 };
 
-angular.module("codify", ['projects.controllers','classes.controllers','properties.controllers','methods.controllers','app.utils'])
+angular.module("codify", ['projects.controllers', 'classes.controllers', 'properties.controllers', 'methods.controllers', 'app.utils', 'firebase'])
     .config(function($routeProvider) {
-    	console.log("route provider config");
+        console.log("route provider config");
         $routeProvider.when('/projects', {
             templateUrl: 'partials/pages/projects.html',
             controller: "ProjectListCtrl"
@@ -16,4 +16,16 @@ angular.module("codify", ['projects.controllers','classes.controllers','properti
         }).otherwise({
             redirectTo: '/projects'
         });
-    });
+    })
+    .controller('AppController', ['$scope', 'angularFireAuth',
+        function($scope, angularFireAuth) {
+        	var ref = new Firebase("http://codify.firebaseio.com/");
+            angularFireAuth.initialize(ref, {
+                scope: $scope,
+                name: "user"
+            });
+            $scope.login = function() {
+            	angularFireAuth.login('facebook');
+            }
+        }
+    ]);
